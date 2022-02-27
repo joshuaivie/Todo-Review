a58031927fc79d5c7d2c372f8525789ba7eec6f4
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-02-27 05:53:56 +0100

    Commit changes

---
+++ src/modules/todoListData.js
@@ -0,0 +1,29 @@
+const TodoList = [
+  {
+    index: 0,
+    description: 'Setup Application Server Environment',
+    completed: true,
+  },
+  {
+    index: 1,
+    description: 'Create New User for CI/CD Agent',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Implement CI/CD Pipeline',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Test Deployment',
+    completed: false,
+  },
+  {
+    index: 4,
+    description: 'Configure Server DNS',
+    completed: false,
+  },
+];
+
+export default TodoList;
